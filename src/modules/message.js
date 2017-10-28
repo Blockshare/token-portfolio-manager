16963f4f95943c54ad3d5579e03afc6aec86c2c9
Casey O'Neill <caseyoneill78@hotmail.com>
2017-10-28 14:12:03 -0700

    creating a central js file where all messaging and copy will live.

---
+++ src/modules/message.js
@@ -0,0 +1,7 @@
+export const message = {
+	appname: 'Blockshare.io',
+	signInButton: 'Sign In with Blockstack',
+	cryptoAssets: 'Cryptoassets',
+	holdings: 'Personal Holdings',
+	holdingMessage: 'There are currently no cryptoassets in your market basket.'
+}
