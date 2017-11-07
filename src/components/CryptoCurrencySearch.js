3ff93314921cb05a4604801d43c53288911a4e6a
Casey O'Neill <caseyoneill78@hotmail.com>
2017-11-07 08:44:00 -0800

    building test file for easier search results.

--- src/components/CryptoCurrencySearch.js
+++ src/components/CryptoCurrencySearch.js
@@ -6,18 +6,56 @@ import { loadCoinList } from '../modules/coin'; // Import loadCoinList 3rd Party
 export default class CurrencySearch extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = {currency: ''};
-		this.handleSearch = this.handleSearch.bind(this);
+		this.state = {value: '', results: 0}
+		this.update = this.update.bind(this);
 	}
 
-	handleSearch(event) {
+	update(event) {
+		const search = event.target.value;
+		const regex = new RegExp(search, "pre");
+		const str = ("pre").text();
+		const pre = document.querySelector('pre')
+
+		highlight(search, regex, pre);
+
+		const matches = document.querySelectorAll('#h1').length;
 		this.setState({
-			currency: event.target.currency
-		});
+			value: event.target.value,
+			results: (matches === str.length + 1) ? 0 : matches
+		})
 	}
 
-	// Call "loadCoinList" and display the coin that is being search for along with relevant information.
-	// render() {
-		// Add Search Code Here...
-	// }
+	render() {
+		return (
+			<div>
+				<SearchBox txt={this.state.value}
+					update={this.update}/>
+				<SearchResults result={this.state.results}/>
+			</div>
+		)
+	}
+}
+
+const SearchBox = (props) => {
+	return (
+		<div id="searchbox">
+		<input type="text" onChange={props.update} />
+		</div>
+	);
+}
+
+const SearchResults = (props) => {
+	return (
+		<div id="results">
+			<p>{props.result} results found</p>
+		</div>
+	)
+}
+
+const highlight = (str, regex, target) => {
+	target.innerHTML = 
+	str.replace(
+		regex,
+		str => `<span> id="hl">${str}</span>`
+	)
 }
