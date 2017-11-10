c2c7790051b632ec24bb10c7a8f9c55645db5dd6
Casey O'Neill <caseyoneill78@hotmail.com>
2017-11-10 08:29:42 -0800

    new copy.

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -9,8 +9,8 @@ export default class NewsList extends React.Component {
   render() {
     return (
       <div className="container">
-        <br />
-        <div className="italic bold">News Feature Coming Soon...</div>
+        <br /><br />
+        <div className="italic bold">Daily News Feature Coming Soon...</div>
       </div>
     );
   }
